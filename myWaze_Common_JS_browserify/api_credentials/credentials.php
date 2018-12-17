d13155d5c9a2901bbc930bf9fad5fcd5b9303f81
account931 <account931@ukr.net>
2018-12-17 18:22:02 +0200

    Update credentials.php

--- myWaze_Common_JS_browserify/api_credentials/credentials.php
+++ myWaze_Common_JS_browserify/api_credentials/credentials.php
@@ -1,8 +1,8 @@
 <?php
 //Credentials for Map Box  API
 
-//$access_token = 'pk.eyJ1IjoiYWNjb3VudDkzMSIsImEiOiJjaXgwOTVuOTEwMGFxMnVsczRwOWx0czhnIn0.YjZ5bpnh6jqTEk7cCJfrzw'; // Client Secret Key
-define("ACCESS_TOKEN", "pk.eyJ1IjoiYWNjb3VudDkzMSIsImEiOiJjaXgwOTVuOTEwMGFxMnVsczRwOWx0czhnIn0.YjZ5bpnh6jqTEk7cCJfrzw");
+//$access_token = 'pk.eyJ1IjoiYWNjb3VudDkzMSIsImdsfsdfdsfs333333dfsdjqTEk7cCJfrzw'; // Client Secret Key
+define("ACCESS_TOKEN", "pk.eyJ1IjoiYWNjbdgdfgdfgfdgfdgdfgdfgdfg3333333Wx0czhnIn0.YjZ5bpnh6jqTEk7cCJfrzw");
 
 
 ?>
